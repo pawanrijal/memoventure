0d0f9a9bcb435cc0d8cf1e609ae985094187a6e2
pawanrijal <pwnrijal@gmail.com>
2023-08-03 20:31:58 +0545

    initialized and finished authentication

---
+++ src/app.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserController } from './user/user.controller';
+import { AuthController } from './auth/auth.controller';
+import { AuthService } from './auth/auth.service';
+import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
+
+@Module({
+  imports: [AuthModule, PrismaModule],
+  controllers: [
+    AppController,
+    UserController,
+    AuthController,
+  ],
+  providers: [AppService, AuthService],
+})
+export class AppModule {}
---
+++ src/auth/auth.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import {
+  JwtModule,
+  JwtService,
+} from '@nestjs/jwt';
+import { jwtConstants } from 'src/constants';
+
+@Module({
+  imports: [
+    PrismaModule,
+    JwtModule.register({
+      global: true,
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService],
+})
+export class AuthModule {}
